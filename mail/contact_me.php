3014be0b80f63024934835b85ffb7c10ef54beb6
悟.生活 <kou_xulei@yeah.net>
2015-12-25 17:42:17 +0800

    更新Email文件

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'xulei.kou@icloud.com'; // Add your email address inbetween the '' replacing xulei.kou@icloud.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: xulei.kou@icloud.com\n"; // This is the email address the generated message will be from. We recommend using something like xulei.kou@icloud.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
