da65bf228664ec6353bc4b98eb3e2d2b632ae56f
悟.生活 <kou_xulei@yeah.net>
2015-12-25 21:35:01 +0800

    修改Email服务

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -18,20 +18,18 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 //######################################################### 
-$smtpserver = "smtp.yeah.net";                           //  SMTP服务器 
-$smtpserverport = 25;                                     //  SMTP服务器端口 
-$smtpusermail = "kou_xulei@yeah.net";                    //  SMTP服务器的用户邮箱 
-$smtpemailto = "kou_xulei@yeah.net";                   //  发送给谁  收件人
-$smtpuser = "kou_xulei@yeah.net";                        //  SMTP服务器的用户帐号 
-$smtppass = "@kou19891116";                              //  SMTP服务器的用户密码 
-$mailsubject = "Website Contact Form:  $name";           //  邮件主题 
-$mailbody = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";          //  邮件内容 
-$mailtype = "HTML";                                      //  邮件格式（HTML/TXT）,TXT为文本邮件 
-########################################################### 
-
-$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);   //这里面的一个true是表示使用身份验证,否则不使用身份验证. 
-$smtp->debug = true;//是否显示发送的调试信息 
-
+$smtpserver = "smtp.yeah.net";                           //  SMTP
+$smtpserverport = 25;                                    //  SMTP
+$smtpusermail = "kou_xulei@yeah.net";                    //  SMTP 
+$smtpemailto = "kou_xulei@yeah.net";                     //
+$smtpuser = "kou_xulei@yeah.net";                        //  SMTP
+$smtppass = "@kou19891116";                              //  SMTP
+$mailsubject = "Website Contact Form:  $name";           // 
+$mailbody = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";      // 
+$mailtype = "HTML";                                      //(HTML/TXT)
+//########################################################### 
+$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass); //
+$smtp->debug = false;                                                   //
 $smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype); 
 return true;
 ?> 
